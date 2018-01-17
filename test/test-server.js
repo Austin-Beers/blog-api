e5502292bb7b4e28f4a09f9ad245240de7146672
Austin Beers <beersa6@students.rowan.edu>
2018-01-17 00:42:28 -0500

    got first two tests to pass

--- test/test-server.js
+++ test/test-server.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Blog Post', function() {
+describe('Blog Posts', function() {
     before(function() {
         return runServer();
       });
@@ -18,7 +18,7 @@ describe('Blog Post', function() {
 it('should list blog items on GET', function() {
 
     return chai.request(app)
-    .get('/blog-api')
+    .get('/blog-post')
     .then(function(res) {
       expect(res).to.have.status(200);
       expect(res).to.be.json;
@@ -29,7 +29,68 @@ it('should list blog items on GET', function() {
       const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
       res.body.forEach(function(item) {
         expect(item).to.be.a('object');
-        expect(item).to.include.keys(expectedKeys);
+        expect(item).to.have.all.keys(expectedKeys);
       });
     });
+});
+
+it('should add a blog item on POST', function() {
+    const newItem = {
+        title: 'foo bar', 
+        content: 'bizz bar bang foo', 
+        author: 'Sally Students'
+};
+   
+  return chai.request(app)
+      .post('/blog-post')
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.all.keys(['id', 'publishDate'].concat(Object.keys(newItem)));
+        expect(res.body.title).to.equal(newItem.title);
+        expect(res.body.content).to.equal(newItem.content);
+        expect(res.body.author).to.equal(newItem.author)
+      });
+  });
+
+  it('should update blog items on PUT', function() {
+    const updateData = {
+        title: 'bar bar baggins', 
+        content: 'fliend flag floot flight', 
+        author: 'Timmy bean'
+    };
+
+    return chai.request(app)
+    
+    .get('/blog-post')
+    .then(function(res) {
+      updateData.id = res.body[0].id;
+
+      return chai.request(app)
+        .put(`/blog-post/${updateData.id}`)
+        .send(updateData);
+    })
+
+    .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.deep.equal(updateData);
+      });
+  });
+
+  it('should delete blog items on DELETE', function() {
+    return chai.request(app)
+
+      .get('/blog-post')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/blog-post/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+      });
+  });
 });
